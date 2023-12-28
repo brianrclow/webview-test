b118ebf689f9acc7907814cd73f97b3011e3fca4
brianrclow <brianrclow@gmail.com>
2023-12-28 10:24:08 -0800

    init

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit } from "@angular/core";
+import { registerElement } from "@nativescript/angular";
+
+registerElement(
+  "HTMLLabel",
+  () => require("@nativescript-community/ui-label").Label
+);
+
+// registerElement(
+//   "AWebView",
+//   () => require("@nativescript-community/ui-webview").AWebView
+// );
 
 @Component({
-  selector: 'Browse',
-  templateUrl: './browse.component.html',
+  selector: "Browse",
+  templateUrl: "./browse.component.html",
 })
 export class BrowseComponent implements OnInit {
+  example_html: string;
   constructor() {
     // Use the component constructor to inject providers.
+    this.example_html =
+      "<h1>Angular</h1><p>This is hopefully an<em>example</em> of a <u>few things</u></p> <ul> <li>a bulleted</li> <li>list</li> <li>italicas</li> <li>and underline</li> </ul> <ol> <li>numbered list</li> </ol> <p>and <strong>boldness too</strong></p>";
   }
 
   ngOnInit(): void {
--- src/app/browse/browse.module.ts
+++ src/app/browse/browse.module.ts
@@ -1,9 +1,11 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
-import { NativeScriptCommonModule } from '@nativescript/angular'
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptCommonModule } from "@nativescript/angular";
 
-import { BrowseRoutingModule } from './browse-routing.module'
-import { BrowseComponent } from './browse.component'
+import { BrowseRoutingModule } from "./browse-routing.module";
+import { BrowseComponent } from "./browse.component";
 
+// import { AWebViewModule } from "@nativescript-community/ui-webview/angular";
+// AWebViewModule
 @NgModule({
   imports: [NativeScriptCommonModule, BrowseRoutingModule],
   declarations: [BrowseComponent],
